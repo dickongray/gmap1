9abc8ffe25b68d84eb7f9efed81c2deaca4f9954
Poofplonker <michaelglenny@gmail.com>
2015-06-15 22:36:59 +1200

    AJAX functions tested and ready. Only need to change the web server access for local machine and you are good to go.

--- custom.js
+++ custom.js
@@ -1,20 +1,8 @@
- // google maps api, with changing markers.
+  // google maps api, with changing markers.
 
- var Op = [
-     ['new address', -36.828512000000000000, 174.654241100000040000, 1],
-     ['No idea <img src="#" alt="img goes here" >', -36.8630231,174.8654693, 1],
-     ['Op', -36.9876769,174.4729328, 2],
-     ['Op', -36.9528412,174.6329212, 3],
-     ['Op', -36.9528412,174.4729328, 3],
-     ['Op', -36.8428412,174.6329212, 3],
-     ['Op', -36.9572412,174.6329212, 2],
-     ['Op', -36.9578412,174.6329212, 2],
-     ['Op', -36.6758412,174.7629212, 1],
-     ['Op', -36.5674412,174.4729328, 1],
-     ['Op', -36.8528412,175.9572912, 1],
-     ['Op', -36.9348412,174.63235462, 2]
-   ];
-// map details - center zoom etc
+    var Op = loadLocationData();
+    var markers = [];
+  // map details - center zoom etc
    var map = new google.maps.Map(document.getElementById('map'), {
       center: new google.maps.LatLng(-36.8406, 174.7400),
       zoom: 10,
@@ -30,12 +18,15 @@ if ( $(window).width() <= 768) {
      });
  }
 }
-// map markers & info window
-var infowindow = new google.maps.InfoWindow();
 
-   var markers = [];
+/*
+  Loads the markers by calling the google Maps API and hides the markers,
+  to be displayed when the tab is clicked.
+*/
+function placeMarkers(Op){
+   var infowindow = new google.maps.InfoWindow();
    var i, newMarker;
-
+   console.log(Op);
    for (i = 0; i < Op.length; i++) {
      newMarker = new google.maps.Marker({
        position: new google.maps.LatLng(Op[i][1], Op[i][2],Op[i][3]),
@@ -46,7 +37,7 @@ var infowindow = new google.maps.InfoWindow();
      newMarker.category = Op[i][3];
      newMarker.setVisible(false);
      markers.push(newMarker);
-// map infowindow on click
+     // map infowindow on click
      google.maps.event.addListener(newMarker, 'click', (function(newMarker, i) {
         return function() {
           infowindow.setContent(Op[i][0]);
@@ -54,12 +45,18 @@ var infowindow = new google.maps.InfoWindow();
         }
       })(newMarker, i));
    }
+ }
+
+/*
+  Upon a tab click, the map will populate
+  the markers of that category. The markers
+  have been preloaded and only need to be made visible.
+*/
 
 function displayMarkers(category) {
      var i;
-
      for (i = 0; i < markers.length; i++) {
-       if (markers[i].category === category) {
+       if (Number(markers[i].category) === category) {
          markers[i].setVisible(true);
        }
        else {
@@ -68,18 +65,47 @@ function displayMarkers(category) {
      }
    }
 
+/*
+  Function takes the csv containing location data
+  and parses it into an array of arrays so that it
+  can be used by google maps.
+*/
 
+function parseLocationData(opText){
+  var Op, opArray;
+  opArray = opText.split("\n");
+  Op = [];
+  for(var i = 0; i < opArray.length; i++){
+
+    // This processing for converting csv to an array of arrays
+    // assumes that there are no commas in any of the input strings.
+    Op.push(opArray[i].split(','));
+  }
+  return Op;
+}
 
 /*
-    This function tells the browser to send
-    a request to the server
+    This function tells the browser to send a request to the server
     to get locations of stores or whatever.
+
+    Executes asynchronously, meaning that while the file is loading
+    the execution will continue and the base map will load.
 */
+
 function loadLocationData(){
 
   //doesn't work for IE 5 or 6.
   var file_request = new XMLHttpRequest();
+  var Op;
+  //I don't know what op means
 
-  file_request.open("GET","locations.csv",true);
+  file_request.onreadystatechange = function() {
+    if (file_request.readyState == 4 && file_request.status == 200){
+      Op = parseLocationData(file_request.responseText);
+      placeMarkers(Op);
+    }
+  }
+  file_request.open("GET","http://localhost:8888/locations.csv",true);
   file_request.send();
+  return Op;
 }
